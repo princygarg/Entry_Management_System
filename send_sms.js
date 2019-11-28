70aec91241422ba7d86b15f0915291af94fd92ea
Princy <32726778+princygarg@users.noreply.github.com>
2019-11-28 12:12:44 +0530

    Update send_sms.js

--- send_sms.js
+++ send_sms.js
@@ -7,7 +7,7 @@ const client = require('twilio')(accountSid, authToken);
 exports.sendSMS = (data, result)=>{
     client.messages.create({
         to: '+91' + data.to,
-        from: 'process.env.AUTH_TOKEN',
+        from: process.env.AUTH_PHONE,
         body:  data.body
     })
     .then((message) => console.log(message.sid));
